f6f3dd6c39b0d740eb2881260b6bc00d86fd8813
Shamith <49478000+iamwatchdogs@users.noreply.github.com>
2022-06-25 21:39:27 +0530

    Update Case_Study.java

--- Exp_1_Basics/Case_Study.java
+++ Exp_1_Basics/Case_Study.java
@@ -17,6 +17,10 @@ public class Case_Study {
 // The outcomes of not following the syntax of a Java Program :-
 
 /*
+
+    public :- public is an access specifier which tells that this method (or) class can be access 
+    ------    anywhere in the program.
+    
     Code without "public" key word :-
         public class Case_Study {
             static void main(String[] args) {
@@ -31,6 +35,10 @@ public class Case_Study {
  */
 
 /*
+
+    static :- static is an keyword used to delcare an static class (or) method, so that was can access
+    ------    them directly ( within the class ) without an instance of class.
+    
     Code without "static" Key word :-
         public class Case_Study {
             public void main(String[] args) {
@@ -44,6 +52,10 @@ public class Case_Study {
  */
 
 /*
+
+    void :- ( in the following method ) void is the return type of the following main() method.
+    ----
+    
     Code without "void" key word :-
         public class Case_Study {
             public static main(String[] args) {
@@ -58,6 +70,9 @@ public class Case_Study {
  */
 
 /*
+    main :- Basically, we need an static method named "main" to execute the program.
+    ----    Since, the program execution begin from the main() method.
+    
     Code without "main" function name :-
         public class Case_Study {
             public static void (String[] args) {
@@ -68,18 +83,4 @@ public class Case_Study {
      Error Message :-
         C:\Users\x\IdeaProjects\first_program\src\com\temp\Case_Study.java:4:23
                     java: <identifier> expected
- */
-
-/*
-    Code with "String [] args" arguments :-
-        public class Case_Study {
-            public static void main() {
-                System.out.println("Test");
-            }
-        }
-
-     Error Message :-
-        Error: Main method not found in class com.temp.Case_Study, please define the main method as:
-                public static void main(String[] args)
-        or a JavaFX application class must extend javafx.application.Application
  */
